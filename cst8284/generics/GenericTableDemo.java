27988ff9d8ac4ecffa562b33cccf87e60d997031
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2018-04-16 14:24:03 -0400

    Ads compares for Strings BasicShapes SolidObjects
    TODO: Improve tabular formatting.

--- cst8284/generics/GenericTableDemo.java
+++ cst8284/generics/GenericTableDemo.java
@@ -2,8 +2,13 @@
  * 
  */
 package cst8284.generics;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+//import cst8284.solidObject.Circle;
+//import cst8284.solidObject.Rectangle;
+//import cst8284.solidObject.Square;
 
 /**
  * @author Manuel Alonso Tarajano (tarajano@gmail.com)
@@ -11,7 +16,9 @@ import java.util.Arrays;
  */
 public class GenericTableDemo {
   
-//  static ArrayList arList;
+  private static String[] myStrings = {"myString",new String("myString"),"myString"};
+  private static ArrayList<BasicShape> basicShapes = new ArrayList<>();
+  private static ArrayList<SolidObject> solidObjects = new ArrayList<>();
   
   public GenericTableDemo() {
   }
@@ -21,7 +28,26 @@ public class GenericTableDemo {
     Table tb = new Table();
     
     // Integers
-    tb.displayEqualityTable(new ArrayList<Integer>(Arrays.asList(1,2,2,1)));
+    // tb.displayEqualityTable(new ArrayList<Integer>(Arrays.asList(1,2,2,1)));
+
+    // Strings 
+    // tb.displayEqualityTable(new ArrayList<String>(Arrays.asList( myStrings )));
+    
+    // BasicShapes
+    /*
+    basicShapes.add(new Circle(3));
+    basicShapes.add(new Square(6));
+    basicShapes.add(new Rectangle(10, 9));
+    basicShapes.add(new Rectangle(3, 6));
+    tb.displayEqualityTable(basicShapes);
+    */
+    
+    // SolidObjects 
+    solidObjects.add(new SolidObject<BasicShape>(new Circle(3), 5));
+    solidObjects.add(new SolidObject<BasicShape>(new Rectangle(10, 9), 2));
+    solidObjects.add(new SolidObject<BasicShape>(new Rectangle(3, 6), 10));
+    solidObjects.add(new SolidObject<BasicShape>(new Square(6)));
+    tb.displayEqualityTable(solidObjects);
     
   }
 
